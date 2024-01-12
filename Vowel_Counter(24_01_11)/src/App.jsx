8ddd97bb938d77d31634f6da5174b0852bae8976
MontaKr <zhoon95@gmail.com>
2024-01-12 18:36:21 +0900

    VOWEL

--- Vowel_Counter(24_01_11)/src/App.jsx
+++ Vowel_Counter(24_01_11)/src/App.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { GlobalStyle, Wrap } from './styles'
 
 const App = () => {
+
+  const [text, setText] = useState('');
+  const [output, setOutput] = useState('');
+
+  const vowelCount = () => {
+    if (text === "") {
+      setOutput("Please enter some text");
+    } else {
+      // optional chaning
+      const count = text.match(/[aeiou]/gi)?.length || 0;
+      setOutput(`The number of vowels is ${count}`);
+    }
+  };
+
   return (
     <>
       <GlobalStyle/>
-      <Wrap></Wrap>
+      <Wrap>
+        <div className="container">
+          <div className="header">
+            <h1>Vowel Counter</h1>
+          </div>
+          <div className="textarea">
+            <textarea 
+            name="vowelinput" 
+            id="textarea" 
+            cols="30" 
+            rows="10" 
+            placeholder='Enter your text' 
+            value={text} 
+            onChange={(e) => setText(e.target.value)}/>
+          </div>
+          <div className="count">
+            <button id="count" onClick={vowelCount}>Calculate Vowels</button>
+          </div>
+          <div className="output">{output}</div>
+        </div>
+      </Wrap>
     </>
   )
 }
--- Vowel_Counter(24_01_11)/src/styles.jsx
+++ Vowel_Counter(24_01_11)/src/styles.jsx
@@ -1,5 +1,57 @@
 import styled, {createGlobalStyle} from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-export const Wrap = styled.div``
+  body {
+    background-color: lightblue;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+`
+
+export const Wrap = styled.div`
+  .container {
+    background-color: #fff;
+    width: 400px;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0,0,0,0.5);
+  }
+
+  .header {
+    margin-bottom: 10px;
+    padding-top: 10px;
+  }
+
+  #textarea {
+    width: 90%;
+    height: 200px;
+    padding: 10px;
+    margin-bottom: 10px;
+    border-radius: 10px;
+    resize: none;
+  }
+
+  button {
+    width: 90%;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: blue;
+    cursor: pointer;
+    color : #fff;
+    margin-bottom: 10px;
+    font-size: 1.2rem;
+  }
+
+  .output {
+    margin-bottom: 10px;
+  }
+`
