cbbde2927426919c6d0255f28d3b89626a478113
MontaKr <zhoon95@gmail.com>
2024-01-20 19:48:12 +0900

    REACT

--- Infinite_Image_Scroll(24_01_15)/src/App.jsx
+++ Infinite_Image_Scroll(24_01_15)/src/App.jsx
@@ -1,13 +1,118 @@
-import React from 'react'
-import { GlobalStyle, Wrap } from './styles'
+import React, { useEffect, useState } from 'react';
+import { GlobalStyle, ImageComponentWrap, Wrap } from './styles';
+import { gsap, ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+// React.memo로 감싼 ImageComponent
+const ImageComponent = React.memo(({ value }) => {
+  return (
+    <ImageComponentWrap>
+      <img src={value.src} alt={value.alt} loading="lazy" />
+    </ImageComponentWrap>
+  );
+});
 
 const App = () => {
+  const [images, setImages] = useState([]);
+  const startCount = 10;
+  let imageNumber = 5;
+
+  // Add new image
+  const addImage = newImage => {
+    setImages(currentImages => [...currentImages, newImage]);
+  };
+
+  useEffect(() => {
+    // Add 10 more images after initial 4 images
+    for (let i = 0; i < startCount; i++) {
+      addImage({
+        src: `https://picsum.photos/300/200?random=${imageNumber}`,
+        alt: `Image${imageNumber}`
+      });
+      imageNumber++;
+    }
+
+    // Add images when scroll reaches 90% of viewport
+    ScrollTrigger.create({
+      trigger: document.body,
+      start: "top top",
+      end: "bottom bottom",
+      markers: true,
+      onUpdate: self => {
+        if (self.progress.toFixed(2) >= 0.9 && self.direction === 1) {
+          addImage({
+            src: `https://picsum.photos/300/200?random=${imageNumber++}`,
+            alt: `Image${imageNumber}`
+          });
+          ScrollTrigger.refresh();
+        }
+      }
+    });
+  }, []);
+
+  // GSAP animation when new image is shown
+  useEffect(() => {
+    images.forEach((_, index) => {
+      const div = document.querySelectorAll('.container > div')[index];
+      const img = div.querySelector('img');
+
+      gsap.fromTo(div,
+        { scale: 0 },
+        {
+          scrollTrigger: {
+            trigger: div,
+            start: "top bottom",
+            end: "top 50%",
+            scrub: true,
+          },
+          scale: 1,
+          ease: "back.out(2)"
+        }
+      );
+
+      gsap.fromTo(img,
+        { opacity: 0, filter: "contrast(5)" },
+        {
+          scrollTrigger: {
+            trigger: div,
+            start: "top bottom",
+            end: "top 50%",
+            scrub: true
+          },
+          opacity: 1,
+          filter: "contrast(1)"
+        }
+      );
+    });
+  }, [images]);
+
   return (
     <>
       <GlobalStyle/>
-      <Wrap></Wrap>
+      <Wrap>
+        <div className="container">
+          {/* first 4 images */}
+          <ImageComponentWrap>
+            <img src="https://picsum.photos/300/200?random=1" alt="" />
+          </ImageComponentWrap>
+          <ImageComponentWrap>
+            <img src="https://picsum.photos/300/200?random=2" alt="" />
+          </ImageComponentWrap>
+          <ImageComponentWrap>
+            <img src="https://picsum.photos/300/200?random=3" alt="" />
+          </ImageComponentWrap>
+          <ImageComponentWrap>
+            <img src="https://picsum.photos/300/200?random=4" alt="" />
+          </ImageComponentWrap>
+          {/* additional images */}
+          {images.map((value, index) => (
+            <ImageComponent key={index} value={value} />
+          ))}
+        </div>
+      </Wrap>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- Infinite_Image_Scroll(24_01_15)/src/styles.jsx
+++ Infinite_Image_Scroll(24_01_15)/src/styles.jsx
@@ -1,5 +1,29 @@
 import styled, {createGlobalStyle} from "styled-components";
 
-export const GlobalStyle = createGlobalStyle``
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0; 
+    background-color: teal; 
+  }
+`
 
-export const Wrap = styled.div``
+export const Wrap = styled.div`
+  .container {
+    width: 320px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+`
+
+export const ImageComponentWrap = styled.div`
+  margin: 20px 10px;
+  background-color: #000;
+  border-radius: 15px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  overflow: hidden;
+
+  img {
+    display: block;
+  }
+`
